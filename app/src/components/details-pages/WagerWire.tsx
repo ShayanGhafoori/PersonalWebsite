1b20fcd218aa95db95e58e824e4b2055ce14a27c
Shayan Ghafoori <ghafoorishayan@gmail.com>
2024-03-07 19:57:49 -0700

    Updated WW Project Page

--- app/src/components/details-pages/WagerWire.tsx
+++ app/src/components/details-pages/WagerWire.tsx
@@ -1,36 +1,44 @@
+import { useState, useEffect } from "react";
+import clsx from "clsx";
+
 const Home = () => {
   return (
-    <div className='flex flex-row justify-evenly h-screen overflow-scroll mt-24'>
-      <div className='pr-6'>
-        <div className='absolute top-60'> Small screen </div>
-        <img
-          className=''
-          src='/images/www.wagerwire.com_small.png'
-          alt='ww-home-large'
-        />
-      </div>
-      <div className='pr-6'>
-        <div className='absolute top-60'> Medium screen </div>
-        <img
-          className=''
-          src='/images/www.wagerwire.com_medium.png'
-          alt='ww-home-large'
-        />
-      </div>
-
-      <div className='pr-6'>
-        <div className='absolute top-60'> Large screen </div>
-        <img
-          className=''
-          src='/images/www.wagerwire.com_large.png'
-          alt='ww-home-large'
-        />
+    <div>
+      <p className='mt-8 text-[12px]'>
+        So here is a brief description of everything
+      </p>
+      <div className='flex flex-row justify-evenly h-[500px] overflow-scroll mt-24'>
+        <div className='pr-6'>
+          <div className='absolute top-72'> Small screen </div>
+          <img
+            className=''
+            src='/images/www.wagerwire.com_small.png'
+            alt='ww-home-large'
+          />
+        </div>
+        <div className='pr-6'>
+          <div className='absolute top-72'> Medium screen </div>
+          <img
+            className=''
+            src='/images/www.wagerwire.com_medium.png'
+            alt='ww-home-large'
+          />
+        </div>
+        <div className='pr-6'>
+          <div className='absolute top-72'> Large screen </div>
+          <img
+            className=''
+            src='/images/www.wagerwire.com_large.png'
+            alt='ww-home-large'
+          />
+        </div>
       </div>
     </div>
   );
 };
 
 const WagerWire = () => {
+  const [subPage, setSubPage] = useState("HOME");
   return (
     <div className='font-semibold'>
       <a
@@ -41,14 +49,80 @@ const WagerWire = () => {
       </a>
       <div className='md:max-w-[500px] lg:max-w-[800px]'>
         <div className='flex flex-row text-[24px] pt-8'>
-          <h2 className='mr-4'> Home Page</h2>
-          <h2 className='mr-4'> Calculator Page</h2>
-          <h2 className='mr-4'> Graph Page</h2>
-          <h2> Community Page</h2>
+          <div className='flex flex-col justify-center items-center mr-4 group'>
+            <button
+              className={clsx(
+                "group-hover:text-white",
+                subPage === "HOME" && "text-white"
+              )}
+              onClick={() => setSubPage("HOME")}
+            >
+              Home Page
+            </button>
+            <hr
+              className={clsx(
+                "border-2 w-full group-hover:border-white",
+                subPage === "HOME" ? "border-white" : "border-black"
+              )}
+            />
+          </div>
+
+          <div className='flex flex-col justify-center items-center mr-4 group'>
+            <button
+              className={clsx(
+                "group-hover:text-white",
+                subPage === "CALC" && "text-white"
+              )}
+              onClick={() => setSubPage("CALC")}
+            >
+              Calculator Page
+            </button>
+            <hr
+              className={clsx(
+                "border-2 w-full group-hover:border-white",
+                subPage === "CALC" ? "border-white" : "border-black"
+              )}
+            />
+          </div>
+
+          <div className='flex flex-col justify-center items-center mr-4 group'>
+            <button
+              className={clsx(
+                "group-hover:text-white",
+                subPage === "GRAPH" && "text-white"
+              )}
+              onClick={() => setSubPage("GRAPH")}
+            >
+              Graph Page
+            </button>
+            <hr
+              className={clsx(
+                "border-2 w-full group-hover:border-white",
+                subPage === "GRAPH" ? "border-white" : "border-black"
+              )}
+            />
+          </div>
+
+          <div className='flex flex-col justify-center items-center group'>
+            <button
+              className={clsx(
+                "group-hover:text-white",
+                subPage === "COMM" && "text-white"
+              )}
+              onClick={() => setSubPage("COMM")}
+            >
+              Community Page
+            </button>
+            <hr
+              className={clsx(
+                "border-2 w-full group-hover:border-white",
+                subPage === "COMM" ? "border-white" : "border-black"
+              )}
+            />
+          </div>
         </div>
-        <hr className='border-black border-2' />
       </div>
-      <Home />
+      {subPage === "HOME" && <Home />}
     </div>
   );
 };
