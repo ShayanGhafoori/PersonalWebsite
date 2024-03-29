ae0b8bf80668770ce67703d8da589323ab77d548
Shayan Ghafoori <ghafoorishayan@gmail.com>
2024-03-29 11:48:22 -0700

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <body className='bg-[#798ECE] h-full min-h-screen bg-scroll px-10 py-10 md:px-20 md:py-20 font-lucida-sans-typewriter'>
-      <HashRouter basename='/'>
+      <HashRouter>
         <Routes>
           <Route path='/' exact={true} element={<Home />} />
           <Route path='/projects' exact={true} element={<Projects />} />
