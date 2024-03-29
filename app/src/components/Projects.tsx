341cf28a8b21a7495040364c36c890b29087a17f
Shayan Ghafoori <ghafoorishayan@gmail.com>
2024-03-28 21:54:08 -0700

    Updated WW section of project page

--- app/src/components/Projects.tsx
+++ app/src/components/Projects.tsx
@@ -33,33 +33,36 @@ const WW = () => {
         proprietary algorithm for valuing sports bets, concurrently developing
         intuitive GUIs that enabled public utilization
       </div>
-      <div className='mb-4'>
-        As a vital member of the data team, I built a proprietary sports betting
-        algorithm, drawing insights from 22 years of diverse betting data
-        spanning multiple sports and bet-types.
-      </div>
 
-      <div className='mb-4'>
-        As a vital member of the engineering team, I spearheaded the development
-        of the <span className='italic font-extralight'> /calculator</span> and
-        <span className='italic font-extralight'> /graph </span> pages of
-        <span className='italic font-extralight'> wagerwire.com </span>. These
-        user-friendly interfaces empower individuals to independently calculate
-        the monetary worth of their bets and visualize value fluctuations over
-        time across various sports, all achieved using the algorithm.
-        Additionally, I created the{" "}
-        <span className='italic font-extralight'> / </span> (home) page--
-        effectively encapsulating WagerWire's mission and the app's features--
-        as well as a vibrant{" "}
-        <span className='italic font-extralight'> /community </span> page that
-        hosts an array of sports-related content, fostering engagement and
-        interaction among users.
-      </div>
+      <ul className='mb-4 list-disc pl-2'>
+        <li className='my-2'>
+          As a vital member of the data team, I built a proprietary sports
+          betting algorithm, drawing insights from 22 years of diverse betting
+          data spanning multiple sports and bet-types.
+        </li>
+        <li className='my-2'>
+          As a vital member of the engineering team, I spearheaded the
+          development of the{" "}
+          <span className='italic font-extralight'> /calculator</span> and
+          <span className='italic font-extralight'> /graph </span> pages of
+          <span className='italic font-extralight'> wagerwire.com </span>. These
+          user-friendly interfaces empower individuals to independently
+          calculate the monetary worth of their bets and visualize value
+          fluctuations over time across various sports, all achieved using the
+          algorithm. Additionally, I created the{" "}
+          <span className='italic font-extralight'> / </span> (home) page--
+          effectively encapsulating WagerWire's mission and the app's features--
+          as well as a vibrant{" "}
+          <span className='italic font-extralight'> /community </span> page that
+          hosts an array of sports-related content, fostering engagement and
+          interaction among users.
+        </li>
+      </ul>
       <a
         href='/details-wagerwire'
-        className='font-bold text-[17px] text-white hover:text-[#E5BAC9] underline pt-4 italic font-normal'
+        className='font-bold text-[17px] text-white hover:text-[#E5BAC9] underline pt-4 italic font-normal underline-offset-2'
       >
-        !!! See more details !!!
+        !! See more details !!
       </a>
     </div>
   );
