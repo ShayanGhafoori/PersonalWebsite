2359754f176bb7e48d00020029d5ad153f438403
Shayan Ghafoori <ghafoorishayan@gmail.com>
2026-02-11 22:59:56 -0800

    changes

--- app/src/App.js
+++ app/src/App.js
@@ -6,13 +6,12 @@ const App = () => {
   ReactGA.initialize("G-4BMF8VVYPD");
   return (
     <body className='bg-[#333333] h-full min-h-screen bg-scroll overflow-x-scroll font-neue-haas-grotesk text-white p-8'>
-      {/* <HashRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' exact={true} element={<Home />} />
           <Route path='/projects' exact={true} element={<Projects />} />
         </Routes>
-      </HashRouter> */}
-      Under Maintanence
+      </HashRouter>
     </body>
   );
 };
